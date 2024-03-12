717590daa78db5a30fb3b8955db6bea5aabc8be8
Carl Vogel <vogel.carl.l@gmail.com>
2024-03-12 17:14:02 +0100

    added Functionality to the Pause Menu buttons

---
+++ Vampire Hunter/Assets/Scripts/SaveAndLoad.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveAndLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SaveLoadExitButton.saveGame == true)
+        {
+            SaveGame();
+            SaveLoadExitButton.saveGame = false;
+        }
+        else if (SaveLoadExitButton.loadGame == true)
+        {
+            LoadGame();
+            SaveLoadExitButton.loadGame = false;
+        }
+    }
+
+    private void LoadGame()
+    {
+        // you still gotta implenent this
+        // need to look what you need
+    }
+
+    private void SaveGame()
+    {
+        // you still gotta implenent this
+        // need to look what you need
+        // will be the same as above
+    }
+}
